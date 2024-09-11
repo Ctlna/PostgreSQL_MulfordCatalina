e1c9324328301af9a6844c2c8899a412524eab92
Catalina <150844717+Ctlna@users.noreply.github.com>
2024-09-11 12:45:55 +0000

    feat: :sparkles: Se continuan las consultas

--- Dia3/Ejercicio1/consultas.sql
+++ Dia3/Ejercicio1/consultas.sql
@@ -28,3 +28,25 @@ where puesto != 'Representante Ventas';
 select nombre_cliente
 from cliente
 where pais = 'España';
+
+--7. Lista con los distintos estados por los que puede pasar un pedido.
+select distinct estado
+from pedido;
+
+/*8. Lista con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en 
+ *cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:*/
+select c.codigo_cliente 
+from cliente c
+left join pago p on p.codigo_cliente = c.codigo_cliente 
+where extract ( year from p.fecha_pago) = 2008;
+
+--9. Lista con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos
+--que no han sido entregados a tiempo.
+select codigo_pedido , codigo_cliente , fecha_esperada , fecha_entrega 
+from pedido
+where fecha_entrega > fecha_esperada ;
+
+--10. Lista con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos
+--cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada.
+
+
